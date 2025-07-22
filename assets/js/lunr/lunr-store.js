edbafb65981b6c7ea817f30ad600daf443da3ae9
RubenSeverino <RubenSeverino@users.noreply.github.com>
2025-07-22 21:53:28 +0000

    deploy: 9a85da514f0d5975008f8228ec327baaf6bb88f6

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,19 @@
-var store = []
+var store = [{
+        "title": "SolarBurner",
+        "excerpt":"Detalles del juego   Es un juego ambientado en el año 2150 donde lideras escuadrones de asalto en un planeta helado…  ","categories": [],
+        "tags": [],
+        "url": "/projects/solarburner/",
+        "teaser": null
+      },{
+        "title": "Solartopia",
+        "excerpt":"Detalles del juego   Es un juego ambientado en el año 2150 donde lideras escuadrones de asalto en un planeta helado…  ","categories": [],
+        "tags": [],
+        "url": "/projects/solartopia/",
+        "teaser": null
+      },{
+        "title": "TheRedMirror",
+        "excerpt":"Detalles del juego   Es un juego ambientado en el año 2150 donde lideras escuadrones de asalto en un planeta helado…  ","categories": [],
+        "tags": [],
+        "url": "/projects/theredmirror/",
+        "teaser": null
+      }]
